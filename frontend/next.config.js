a9a4ee6801fbed3ea390c662f2e255ac26f5d081
nikolastojadinov <nikolastojadinov@yahoo.co.uk>
2025-10-28 17:33:40 +0100

    Add image domains and formats to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     typedRoutes: true
+  },
+  images: {
+    domains: [
+      'i.ytimg.com',                     // YouTube thumbnails
+      'ofkfygqrfenctzitigae.supabase.co' // Supabase Storage
+    ],
+    formats: ['image/avif', 'image/webp']
   }
 };
 
